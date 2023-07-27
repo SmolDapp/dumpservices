c77bf8ccf6e2465c87cdda7d39510bb4cc74f9fc
Major <major@ycorpo.com>
2023-07-27 14:15:03 +0200

    fix createPublicClient

--- utils/wagmiUtils.ts
+++ utils/wagmiUtils.ts
@@ -1,53 +1,55 @@
+import {localhost} from 'utils/wagmiNetworks';
 import {createPublicClient, http} from 'viem';
-import {arbitrum, fantom, gnosis, localhost, optimism, polygon, polygonZkEvm} from 'viem/chains';
-import {mainnet, type PublicClient} from 'wagmi';
+import {arbitrum, fantom, gnosis, optimism, polygon, polygonZkEvm} from 'viem/dist/types/chains';
+import {mainnet} from 'wagmi';
 
+import type {Chain, PublicClient} from 'viem';
 
 export function getClient(chainID: number): PublicClient {
 	if (chainID === 1337) {
 		return createPublicClient({
 			chain: localhost,
-			transport: http('http://localhost:8545')
+			transport: http(process.env.JSON_RPC_URL?.[1] || mainnet.rpcUrls.public.http[0])
 		});
 	}
 	if (chainID === 10) {
 		return createPublicClient({
-			chain: optimism,
-			transport: http('https://mainnet.optimism.io')
+			chain: optimism as Chain,
+			transport: http(process.env.JSON_RPC_URL?.[10] || optimism.rpcUrls.public.http[0])
 		});
 	}
 	if (chainID === 100) {
 		return createPublicClient({
 			chain: gnosis,
-			transport: http('https://rpc.xdaichain.com')
+			transport: http(process.env.JSON_RPC_URL?.[100] || gnosis.rpcUrls.public.http[0])
 		});
 	}
 	if (chainID === 137) {
 		return createPublicClient({
 			chain: polygon,
-			transport: http(`https://polygon-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_KEY}`)
+			transport: http(process.env.JSON_RPC_URL?.[137] || polygon.rpcUrls.public.http[0])
 		});
 	}
 	if (chainID === 250) {
 		return createPublicClient({
 			chain: fantom,
-			transport: http('https://rpc3.fantom.network')
+			transport: http(process.env.JSON_RPC_URL?.[250] || fantom.rpcUrls.public.http[0])
 		});
 	}
 	if (chainID === 1101) {
 		return createPublicClient({
 			chain: polygonZkEvm,
-			transport: http('https://zkevm-rpc.com')
+			transport: http(process.env.JSON_RPC_URL?.[1101] || polygonZkEvm.rpcUrls.public.http[0])
 		});
 	}
 	if (chainID === 42161) {
 		return createPublicClient({
 			chain: arbitrum,
-			transport: http(`https://arbitrum-mainnet.infura.io/v3/${process.env.INFURA_PROJECT_ID}`)
+			transport: http(process.env.JSON_RPC_URL?.[42161] || arbitrum.rpcUrls.public.http[0])
 		});
 	}
 	return createPublicClient({
 		chain: mainnet,
-		transport: http(`https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_KEY}`)
+		transport: http(process.env.JSON_RPC_URL?.[1] || mainnet.rpcUrls.public.http[0])
 	});
 }
